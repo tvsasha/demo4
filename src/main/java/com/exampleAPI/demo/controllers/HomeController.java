1d2f24f7f266912da0b5e8dc1f10ffd9996329f2
tvsasha <[tvsasha2006@gmail])>
2024-05-08 08:18:43 +0300

    11

---
+++ src/main/java/com/exampleAPI/demo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.exampleAPI.demo.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller@RequestMapping("/rest/home")public class HomeController {
+    @ResponseBody
+    @RequestMapping(value = "", method=RequestMethod.GET)
+    public String hello(){
+        return "Answer endpoint rest/home";
+    }
+
+}
