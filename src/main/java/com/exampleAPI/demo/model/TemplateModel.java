bf227a6ca5c64bd2ef17370f56a1a5ec79751073
tvsasha <[tvsasha2006@gmail])>
2024-05-08 08:39:29 +0300

    12

---
+++ src/main/java/com/exampleAPI/demo/model/TemplateModel.java
@@ -0,0 +1,12 @@
+package com.exampleAPI.demo.model;
+
+public class TemplateModel {
+    private  String message;
+    private String time;
+    public String getMessage(){
+    return  message;
+    }
+    public void  setMessage(string message) {
+        this.message = message;
+    }
+}
